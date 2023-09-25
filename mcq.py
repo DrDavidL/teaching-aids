31b25301ec1c7f15d8889a9fa170dcb2040c5500
DrDavidL <92898146+DrDavidL@users.noreply.github.com>
2023-09-25 10:06:20 -0500

    disclaimer update

--- mcq.py
+++ mcq.py
@@ -193,7 +193,8 @@ if check_password():
     st.info("""Embeddings, i.e., reading your file(s) and converting words to numbers, are created using an OpenAI [embedding model](https://platform.openai.com/docs/guides/embeddings/what-are-embeddings) and indexed for searching. Then,
             your selected model (e.g., gpt-3.5-turbo-16k) is used to answer your questions.""")
     st.warning("""Some PDFs are images and not formatted text. If the summary feature doesn't work, you may first need to convert your PDF
-            using Adobe Acrobat. Choose: `Scan and OCR`,`Enhance scanned file` \n   Save your updates, upload and voilà, you can chat with your PDF!""")
+            using Adobe Acrobat. Choose: `Scan and OCR`,`Enhance scanned file` \n   Alternatively, sometimes PDFs are created with 
+            unusual fonts or LaTeX symbols. Export the file to Word, re-save as a PDF and try again. Save your updates, upload and voilà, you can chat with your PDF! """)
     uploaded_files = []
     # os.environ["OPENAI_API_KEY"] = st.secrets["OPENAI_API_KEY"]
 
